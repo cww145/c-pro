faf7d7bca4391fa5c638928e37e68bf746b47b2d
cww <chiwawa.str@gmail.com>
2017-08-27 02:06:03 +0900

    macro

---
+++ template/macro/macro.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define REP(i,n) for(int i=0; i<(n); i++)
+#define REP2(i,x,n) for(int i=x; i<(n); i++)
+#define ALL(n) begin(n),end(n)
+#define SORT(x) sort((x).begin(),(x).end(),greater<int>())
+using LL=long long;
+using ULL=unsigned long long;
+using PII=pair<int,int>;
+const int INF = numeric_limits<int>::max();
+struct cww{cww(){ios::sync_with_stdio(false);cin.tie(0);}}star;
+vector<vector<int>> memo( 10000 + 1, vector<int>( 10000 + 1, -1 ) );
+int main()
+{
+
+	return 0;
+}
